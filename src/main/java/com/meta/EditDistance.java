6566b27d8167546bfb75c432adb6c3b6039516b7
listree <li@listree.com>
2024-03-24 12:32:51 -0700

    Meta - Maximum Level Sum, Remove Duplicates, k closest elements

---
+++ src/main/java/com/meta/EditDistance.java
@@ -0,0 +1,18 @@
+package com.meta;
+
+/**
+ * https://leetcode.com/problems/edit-distance/
+ * 72. Edit Distance Medium TODO
+ * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+ * You have the following three operations permitted on a word:
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ */
+public class EditDistance {
+
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+
+}
